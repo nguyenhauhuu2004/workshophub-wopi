79457efb5b9fcfbad29b15450827f5d3e87af286
nguyenhauhuu2004 <nguyenhauhuu2004@gmail.com>
2026-07-15 23:19:06 +0700

    .

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL,
+    import.meta.env.VITE_API_BASE_URL,
+
+    // import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
 
--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -5,9 +5,10 @@ const ChatAppPage = () => {
   const user = useAuthStore((s) => s.user);
 
   return (
-    <div>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       {user?.username}
       <Logout />
+      <img className="h-auto max-w-full rounded-4xl" src="IMG_7919.jpeg" alt="image description" />
     </div>
   );
 };
