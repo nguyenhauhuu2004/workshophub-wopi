99cfa8b2fea1c0a04871ec142fa45ccc69c3a622
nguyenhauhuu2004 <nguyenhauhuu2004@gmail.com>
2026-07-22 22:11:26 +0700

    workshop

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,11 +2,10 @@ import { useAuthStore } from "@/stores/useAuthStore";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    //  import.meta.env.VITE_API_URL,
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  // import.meta.env.MODE === "development"
+  //   ? "http://localhost:5001/api"
+  //   : "/api",
   withCredentials: true,
 });
 
